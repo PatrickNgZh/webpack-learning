f6efb8a73548872259f04d4c28044c5b0877dad8
PatrickNg <patrickngzh@outlook.com>
2021-01-24 00:07:26 +0800

    add webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ function add (x, y) {
   return x + y
 }
 
-console.log(add(2, 3))
+console.log(add(4, 4))
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
     loader 1.download  2.use
     plugin 1.download  2.import  3.use
  */
+const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -21,4 +22,17 @@ module.exports = {
       template: './src/index.html',
     })
   ],
+  // dev server build in memory
+  devServer: {
+    // due to html-webpack-plugin, contentBase is invalid
+    // contentBase: resolve(__dirname, 'dist'),
+    // send .html.zip for saving on bandwidth and download time
+    compress: true,
+    // default port
+    port: 8080,
+    open: true
+  },
+  // link packed code to original code
+  // recommend development mode to use eval-cheap-module-source-map
+  devtool: 'eval-cheap-module-source-map'
 }
